457d9802ac3378ea0986d62bf0476b161fc18b61
bilalshahzad139 <bilal.shahzad@ibexglobal.com>
2017-11-08 13:36:21 +0500

    Task 6 (Adding two numbers)

---
+++ Task6/Task6.php
@@ -0,0 +1,33 @@
+<?php
+	//Objective: Sending Data Back to Server
+	
+	
+	//Step 1: Check if Button is pressed to submit form	
+	if(isset($_POST["btnAdd"]) == true)
+	{
+	//Step 2: Get values of both textboxes
+		$num1= ;
+		$num2= ;
+		
+	//Step 3: Add both numbers and display result in result text box
+			
+	}
+?>
+<html>
+<head>
+<title>Task6</title>
+</head>
+<body>
+	
+	<form action="" method="POST">
+		First:<input type="text" name="txtFirstNumber" /> <br>
+		Second:<input type="text" name="txtSecondNumber" /><br>
+		<input type="submit" value="OK" name="btnAdd" /> <br>
+		Result:<input type="text" name="txtResult" value=""/>
+	</form>
+	
+    </tbody>
+	</table>
+
+</body>
+</html>
---
+++ Task6/Task6_Sol.php
@@ -0,0 +1,33 @@
+<?php
+	//Objective: Sending Data Back to Server
+	
+	$sum = 0;
+	//Step 1: Check if Button is pressed to submit form	
+	if(isset($_POST["btnAdd"]) == true)
+	{
+	//Step 2: Get values of both textboxes
+		$num1= $_REQUEST["txtFirstNumber"];
+		$num2= $_REQUEST["txtSecondNumber"];
+		
+	//Step 3: Add both numbers and display result in result text box
+		$sum = $num1 + $num2;		
+	}
+?>
+<html>
+<head>
+<title>Task6_Sol</title>
+</head>
+<body>
+	
+	<form action="" method="POST">
+		First:<input type="text" name="txtFirstNumber" /> <br>
+		Second:<input type="text" name="txtSecondNumber" /><br>
+		<input type="submit" value="OK" name="btnAdd" /> <br>
+		Result:<input type="text" name="txtResult" value="<?php echo $sum ?>"/>
+	</form>
+	
+    </tbody>
+	</table>
+
+</body>
+</html>
