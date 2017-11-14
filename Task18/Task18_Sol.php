c0c2186a189d5afa130c4a7aa7c46af3d2d5df71
bilalshahzad139 <bilal.shahzad@ibexglobal.com>
2017-11-14 13:18:45 +0500

    Getting data from MySQL DB

---
+++ Task18/Task18_Sol.php
@@ -0,0 +1,29 @@
+
+ <?php require('conn.php'); ?>
+ 
+<?php 
+
+		/*
+		Objective: Reading Data (countries) from Database & Displaying it
+		*/
+			
+		//Step-1: Write SQL query
+		$sql = "SELECT id,name FROM country";
+		
+		//Step-2: Execute SQL Query
+		$result = mysqli_query($conn, $sql);
+		
+		//Step-3: Get count of result
+		$recordsFound = mysqli_num_rows($result);			
+		if ($recordsFound > 0) {
+			
+			//Step-4: Iterate row by row 
+			while($row = mysqli_fetch_assoc($result)) {
+			
+				$id = $row["id"];
+				$name = $row["name"];
+				
+				//Step-5: Display values
+				echo "ID: $id, Name:$name";
+			}
+		}	
